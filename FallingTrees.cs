74c5804040112a8820d3b787984f82c24b16e2d8
Imapo <83175739+Imapo@users.noreply.github.com>
2026-08-02 14:37:55 +0500

    Update FallingTrees.cs

--- FallingTrees.cs
+++ FallingTrees.cs
@@ -1,6 +1,6 @@
 using Terraria.ModLoader;
 
-namespace FallingTrees
+namespace ImapoFallingTrees
 {
     // Главный класс мода. tModLoader находит контент (ModTile, ModProjectile, GlobalTile
     // и т.д.) автоматически по всем классам в сборке — здесь почти ничего писать не нужно.
